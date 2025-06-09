1a7186caf70f728e30c590b9896308e2e12a7730
DylDev <dylanb92010@gmail.com>
2025-06-09 10:24:48 -0400

    Add logging to `xdd` cog

--- cogs/xdd.py
+++ cogs/xdd.py
@@ -1,5 +1,8 @@
 from discord.ext import commands
 import discord
+import logging
+
+logger = logging.getLogger()
 
 class xdd(commands.Cog):
     def __init__(self, bot):
@@ -12,6 +15,7 @@ class xdd(commands.Cog):
             member = ctx.author
         
         await ctx.send(f"<:xdx:1195115294696415352>")
+        logger.info(f"[bold]{member.name}[/bold] used the xdd command")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(xdd(bot))
