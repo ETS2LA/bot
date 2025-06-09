80cbe2d37316776a585512d29608c465c02fddcd
DylDev <dylanb92010@gmail.com>
2025-06-09 10:27:07 -0400

    Major overhaul of the logger for ease of use

--- utils/logger.py
+++ utils/logger.py
@@ -1,29 +1,54 @@
 import utils.variables as variables
+
+from rich.highlighter import NullHighlighter, RegexHighlighter
+from rich.logging import RichHandler
+from rich.console import Console
+import inspect
 import logging
 import os
+import re
 
-def setup_logger():
-    # Custom logger setup
-    logger = logging.getLogger(variables.LOGGER_NAME)
+class level_names:
+    '''Shortened level names for logging'''
+    debug = 'DBG'
+    info = 'INF'
+    warning = 'WRN'
+    error = 'ERR'
+    critical = 'CRT'
+
+    def level(self, level_name):
+        '''Returns the shortened level name for the given level name'''
+        return getattr(self, level_name.lower())
+
+class FileFormatter(logging.Formatter):
+    '''Logging formatter class for file output'''
+    def __init__(self):
+        super().__init__(datefmt="%m-%d-%y %H:%M:%S")
+
+    def format(self, record):
+        timestamp = self.formatTime(record, self.datefmt) # Timestamp
+        level_name = f"[{level_names().level(record.levelname)}]" # Three character level name
+        message = re.sub(r'\[.*?\]', '', record.getMessage()) # Remove rich markup
+
+        # Get filename and line number
+        filename = os.path.basename(getattr(record, 'custom_filename', record.filename))
+        lineno = getattr(record, 'custom_lineno', record.lineno)
+
+        return f"{timestamp} {level_name} ({filename}:{lineno}) {message}" # Return formatted message
+
+def setup_global_logging():
+    console = Console()
+    logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    # Console handler
-    console_handler = logging.StreamHandler()
-    logger.addHandler(console_handler)
+    rich_handler = RichHandler(markup=True, show_path=False, console=console)
+    logger.addHandler(rich_handler)
 
-    # File handler
-    open(variables.LOG_FILE, 'w').close() # Clear the log file
+    if os.path.exists(variables.LOG_FILE):
+        os.remove(variables.LOG_FILE)
     file_handler = logging.FileHandler(variables.LOG_FILE)
+    file_handler.setFormatter(FileFormatter())
     logger.addHandler(file_handler)
-
-    # Redirect the discordpy logger to the custom logger
-    discord_logger = logging.getLogger('discord')
-    discord_logger.setLevel(logging.INFO)
-    discord_logger.handlers = []
-    discord_logger.propagate = True
-    logging.root.setLevel(logging.INFO)
-    logging.root.addHandler(console_handler)
-    logging.root.addHandler(file_handler)
 
     logger.info("Logger initialized")
     return logger
